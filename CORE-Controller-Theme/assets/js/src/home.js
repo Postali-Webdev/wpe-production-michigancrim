d0bfaf274280e60b5911ec6fd8ef74138be73bef
Eric Schumacher <webdev@postali.com>
2026-06-10 10:12:00 -0400

    Additional updates to new theme files

--- CORE-Controller-Theme/assets/js/src/home.js
+++ CORE-Controller-Theme/assets/js/src/home.js
@@ -11,49 +11,8 @@
 jQuery( function ( $ ) {
 	"use strict";
 
-    var counterAnimated = false;
-
-    jQuery(window).on('scroll', function() {
-        if (counterAnimated) {
-            return;
-        }
-
-        var counterElement = jQuery("#counter2");
-        var elementPosition = counterElement.offset().top;
-        var scrollPosition = jQuery(window).scrollTop() + window.innerHeight;
-
-        if (scrollPosition > elementPosition) {
-            counterAnimated = true;
-
-            jQuery('.alert-2').addClass('fadeup');
-            jQuery('.half-donut-chart').addClass('fadeup');
-            jQuery('.half-donut-chart').find('.half-donut-fill').addClass('animate');
-
-            jQuery(".counter-value2").each(function() {
-                var $counter = jQuery(this);
-                var targetCount = $counter.attr("data-count");
-
-                jQuery({
-                    countNum: $counter.text()
-                }).animate({
-                    countNum: targetCount
-                }, {
-                    duration: 2e3,
-                    easing: "swing",
-                    step: function() {
-                        $counter.text(Math.floor(this.countNum));
-                    },
-                    complete: function() {
-                        $counter.text(this.countNum);
-                    }
-                });
-            });
-        }
-    });
-
     $('.tabs-container .main-content .tab-content:first-of-type').addClass('active');
-
-
+    
     $('.main-buttons span').click(function() {
 
         // Check for active
